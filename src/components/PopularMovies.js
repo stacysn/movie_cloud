db97ac837d52763718cd6f92d5476f49818a213b
stacysn <stacysuen5@gmail.com>
2019-02-09 21:40:31 -0800

    added components

---
+++ src/components/PopularMovies.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class PopularMovies extends Component {
+  render() {
+    return (
+      <div>
+        This works!
+      </div>
+    )
+  }
+}
+
+export default PopularMovies;
